dc596a46011801c1cc49783f12a87d5ac7d628e8
rnicolasp <rnicolasp@esliceu.net>
2026-01-26 19:55:17 +0100

    css

---
+++ src/main/java/com/example/practicatres/controllers/mainController.java
@@ -0,0 +1,13 @@
+package com.example.practicatres.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
